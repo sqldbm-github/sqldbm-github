e78ca4d5d3c6a8c41d4f7a39269c3cb1d29134f4
sqldbm-github <78134676+sqldbm-github@users.noreply.github.com>
2021-09-01 10:55:17 -0700

    Generated by SqlDBM: AdventureWorks - EDW - v1557 by "support@sqldbm.com"

---
+++ 09/01/2021 10:55:16 AM/AdventureWorks - EDW_Demo Version_v1557_, Dev vs Beta.sql
@@ -0,0 +1,29 @@
+-- *********************** SqlDBM: Snowflake ************************
+-- ******************************************************************
+
+CREATE FILE FORMAT "AdventureWorksDW.EDW.FILEFORMAT_431"
+    TYPE = JSON;
+
+DROP FILE FORMAT "ADVENTUREWORKSDW_BETA.EDW.FILEFORMAT_431";
+
+CREATE SCHEMA "AdventureWorksDW.EDW";
+
+DROP SCHEMA "ADVENTUREWORKSDW_BETA.EDW";
+
+ALTER TABLE "EDW"."DimOrganization"
+    SET COMMENT = 'Dimension for Organizational Data';
+
+ALTER TABLE "EDW"."FactFinance"
+    SET COMMENT = 'Facts for Finance';
+
+CREATE TABLE "EDW"."FactProductInventory"
+(
+ "ProductKey"   number(38,0) NOT NULL,
+ "DateKey"      number(38,0) NOT NULL,
+ "MovementDate" date NOT NULL,
+ "UnitCost"     number(38,0) NOT NULL,
+ "UnitsIn"      number(38,0) NOT NULL,
+ "UnitsOut"     number(38,0) NOT NULL,
+ "UnitsBalance" number(38,0) NOT NULL,
+ CONSTRAINT "FK_FactProductInventory_443" FOREIGN KEY ( "DateKey" ) REFERENCES "EDW"."DimDate" ( "DateKey" )
+);
